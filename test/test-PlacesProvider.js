7eb1fa7c048fd66e5ed48a9bb2b5817f49a8c01b
Olivier Yiptong <olivier@olivieryiptong.com>
2016-02-09 09:31:01 -0500

    back to Tasks

--- test/test-PlacesProvider.js
+++ test/test-PlacesProvider.js
@@ -1,4 +1,4 @@
-/* globals XPCOMUtils, NetUtil, PlacesUtils */
+/* globals XPCOMUtils, NetUtil, PlacesUtils, Task */
 "use strict";
 
 const {before} = require("sdk/test/utils");
@@ -7,6 +7,7 @@ const {PlacesTestUtils} = require("./lib/PlacesTestUtils");
 const {Ci, Cu} = require("chrome");
 
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
+Cu.import("resource://gre/modules/Task.jsm");
 
 XPCOMUtils.defineLazyModuleGetter(this, "PlacesUtils",
                                   "resource://gre/modules/PlacesUtils.jsm");
@@ -30,169 +31,183 @@ exports.test_LinkChecker_securityCheck = function(assert) {
   }
 };
 
-exports.test_Links_getTopFrecentSites = function*(assert) {
-  let provider = PlacesProvider.links;
+exports.test_Links_getTopFrecentSites = function(assert, done) {
+  Task.spawn(function*() {
+    let provider = PlacesProvider.links;
 
-  let links = yield provider.getTopFrecentSites();
-  assert.equal(links.length, 0, "empty history yields empty links");
+    let links = yield provider.getTopFrecentSites();
+    assert.equal(links.length, 0, "empty history yields empty links");
 
-  // add a visit
-  let testURI = NetUtil.newURI("http://mozilla.com");
-  yield PlacesTestUtils.addVisits(testURI);
+    // add a visit
+    let testURI = NetUtil.newURI("http://mozilla.com");
+    yield PlacesTestUtils.addVisits(testURI);
 
-  links = yield provider.getTopFrecentSites();
-  assert.equal(links.length, 1, "adding a visit yields a link");
-  assert.equal(links[0].url, testURI.spec, "added visit corresponds to added url");
+    links = yield provider.getTopFrecentSites();
+    assert.equal(links.length, 1, "adding a visit yields a link");
+    assert.equal(links[0].url, testURI.spec, "added visit corresponds to added url");
+  }).then(done);
 };
 
-exports.test_Links_getTopFrecentSites_Order = function*(assert) {
-  let provider = PlacesProvider.links;
-  let {
-    TRANSITION_TYPED,
-    TRANSITION_LINK
-  } = PlacesUtils.history;
+exports.test_Links_getTopFrecentSites_Order = function(assert, done) {
+  Task.spawn(function*() {
+    let provider = PlacesProvider.links;
+    let {
+      TRANSITION_TYPED,
+      TRANSITION_LINK
+    } = PlacesUtils.history;
 
-  function timeDaysAgo(numDays) {
-    let now = new Date();
-    return now.getTime() - (numDays * 24 * 60 * 60 * 1000);
-  }
+    function timeDaysAgo(numDays) {
+      let now = new Date();
+      return now.getTime() - (numDays * 24 * 60 * 60 * 1000);
+    }
 
-  let timeEarlier = timeDaysAgo(0);
-  let timeLater = timeDaysAgo(2);
+    let timeEarlier = timeDaysAgo(0);
+    let timeLater = timeDaysAgo(2);
 
-  let visits = [
-    // frecency 200
-    {uri: NetUtil.newURI("https://mozilla1.com/0"), visitDate: timeEarlier, transition: TRANSITION_TYPED},
-    // sort by url, frecency 200
-    {uri: NetUtil.newURI("https://mozilla2.com/1"), visitDate: timeEarlier, transition: TRANSITION_TYPED},
-    // sort by last visit date, frecency 200
-    {uri: NetUtil.newURI("https://mozilla3.com/2"), visitDate: timeLater, transition: TRANSITION_TYPED},
-    // sort by frecency, frecency 10
-    {uri: NetUtil.newURI("https://mozilla4.com/3"), visitDate: timeLater, transition: TRANSITION_LINK},
-  ];
+    let visits = [
+      // frecency 200
+      {uri: NetUtil.newURI("https://mozilla1.com/0"), visitDate: timeEarlier, transition: TRANSITION_TYPED},
+      // sort by url, frecency 200
+      {uri: NetUtil.newURI("https://mozilla2.com/1"), visitDate: timeEarlier, transition: TRANSITION_TYPED},
+      // sort by last visit date, frecency 200
+      {uri: NetUtil.newURI("https://mozilla3.com/2"), visitDate: timeLater, transition: TRANSITION_TYPED},
+      // sort by frecency, frecency 10
+      {uri: NetUtil.newURI("https://mozilla4.com/3"), visitDate: timeLater, transition: TRANSITION_LINK},
+    ];
 
-  let links = yield provider.getTopFrecentSites();
-  assert.equal(links.length, 0, "empty history yields empty links");
-  yield PlacesTestUtils.addVisits(visits);
+    let links = yield provider.getTopFrecentSites();
+    assert.equal(links.length, 0, "empty history yields empty links");
+    yield PlacesTestUtils.addVisits(visits);
 
-  links = yield provider.getTopFrecentSites();
-  assert.equal(links.length, visits.length, "number of links added is the same as obtain by getTopFrecentSites");
-  for (let i = 0; i < links.length; i++) {
-    assert.equal(links[i].url, visits[i].uri.spec, "links are obtained in the expected order");
-  }
+    links = yield provider.getTopFrecentSites();
+    assert.equal(links.length, visits.length, "number of links added is the same as obtain by getTopFrecentSites");
+    for (let i = 0; i < links.length; i++) {
+      assert.equal(links[i].url, visits[i].uri.spec, "links are obtained in the expected order");
+    }
+  }).then(done);
 };
 
-exports.test_Links_onLinkChanged = function*(assert) {
-  let provider = PlacesProvider.links;
-  provider.init();
-  assert.equal(true, true);
+exports.test_Links_onLinkChanged = function(assert, done) {
+  Task.spawn(function*() {
+    let provider = PlacesProvider.links;
+    provider.init();
+    assert.equal(true, true);
 
-  let url = "https://example.com/onFrecencyChanged1";
-  let linkChangedMsgCount = 0;
+    let url = "https://example.com/onFrecencyChanged1";
+    let linkChangedMsgCount = 0;
 
-  let linkChangedPromise = new Promise(resolve => {
-    let handler = (_, link) => { // jshint ignore:line
-      /* There are 3 linkChanged events:
-       * 1. visit insertion (-1 frecency by default)
-       * 2. frecency score update (after transition type calculation etc)
-       * 3. title change
-       */
-      if (link.url === url) {
-        assert.equal(link.url, url, `expected url on linkChanged event`);
-        linkChangedMsgCount += 1;
-        if (linkChangedMsgCount === 3) {
-          assert.ok(true, `all linkChanged events captured`);
-          provider.off("linkChanged", this);
-          resolve();
+    let linkChangedPromise = new Promise(resolve => {
+      let handler = (_, link) => { // jshint ignore:line
+        /* There are 3 linkChanged events:
+         * 1. visit insertion (-1 frecency by default)
+         * 2. frecency score update (after transition type calculation etc)
+         * 3. title change
+         */
+        if (link.url === url) {
+          assert.equal(link.url, url, `expected url on linkChanged event`);
+          linkChangedMsgCount += 1;
+          if (linkChangedMsgCount === 3) {
+            assert.ok(true, `all linkChanged events captured`);
+            provider.off("linkChanged", this);
+            resolve();
+          }
         }
-      }
-    };
-    provider.on("linkChanged", handler);
-  });
+      };
+      provider.on("linkChanged", handler);
+    });
 
-  // add a visit
-  let testURI = NetUtil.newURI(url);
-  yield PlacesTestUtils.addVisits(testURI);
-  yield linkChangedPromise;
-
-  provider.uninit();
-};
-
-exports.test_Links_onClearHistory = function*(assert) {
-  let provider = PlacesProvider.links;
-  provider.init();
-
-  let clearHistoryPromise = new Promise(resolve => {
-    let handler = () => {
-      assert.ok(true, `clearHistory event captured`);
-      provider.off("clearHistory", handler);
-      resolve();
-    };
-    provider.on("clearHistory", handler);
-  });
-
-  // add visits
-  for (let i = 0; i <= 10; i++) {
-    let url = `https://example.com/onClearHistory${i}`;
+    // add a visit
     let testURI = NetUtil.newURI(url);
     yield PlacesTestUtils.addVisits(testURI);
-  }
-  yield PlacesTestUtils.clearHistory();
-  yield clearHistoryPromise;
-  provider.uninit();
+    yield linkChangedPromise;
+
+    provider.uninit();
+  }).then(done);
 };
 
-exports.test_Links_onDeleteURI = function*(assert) {
-  let provider = PlacesProvider.links;
-  provider.init();
+exports.test_Links_onClearHistory = function(assert, done) {
+  Task.spawn(function*() {
+    let provider = PlacesProvider.links;
+    provider.init();
 
-  let testURL = "https://example.com/toDelete";
+    let clearHistoryPromise = new Promise(resolve => {
+      let handler = () => {
+        assert.ok(true, `clearHistory event captured`);
+        provider.off("clearHistory", handler);
+        resolve();
+      };
+      provider.on("clearHistory", handler);
+    });
 
-  let deleteURIPromise = new Promise(resolve => {
-    let handler = (_, {url}) => { // jshint ignore:line
-      assert.equal(testURL, url, "deleted url and expected url are the same");
-      provider.off("deleteURI", handler);
-      resolve();
-    };
-
-    provider.on("deleteURI", handler);
-  });
-
-  let testURI = NetUtil.newURI(testURL);
-  yield PlacesTestUtils.addVisits(testURI);
-  yield PlacesUtils.history.remove(testURL);
-  yield deleteURIPromise;
-  provider.uninit();
+    // add visits
+    for (let i = 0; i <= 10; i++) {
+      let url = `https://example.com/onClearHistory${i}`;
+      let testURI = NetUtil.newURI(url);
+      yield PlacesTestUtils.addVisits(testURI);
+    }
+    yield PlacesTestUtils.clearHistory();
+    yield clearHistoryPromise;
+    provider.uninit();
+  }).then(done);
 };
 
-exports.test_Links_onManyLinksChanged = function*(assert) {
-  let provider = PlacesProvider.links;
-  provider.init();
+exports.test_Links_onDeleteURI = function(assert, done) {
+  Task.spawn(function*() {
+    let provider = PlacesProvider.links;
+    provider.init();
 
-  let promise = new Promise(resolve => {
-    let handler = () => {
-      assert.ok(true);
-      provider.off("manyLinksChanged", handler);
-      resolve();
-    };
+    let testURL = "https://example.com/toDelete";
 
-    provider.on("manyLinksChanged", handler);
-  });
+    let deleteURIPromise = new Promise(resolve => {
+      let handler = (_, {url}) => { // jshint ignore:line
+        assert.equal(testURL, url, "deleted url and expected url are the same");
+        provider.off("deleteURI", handler);
+        resolve();
+      };
 
-  let testURL = "https://example.com/toDelete";
-  let testURI = NetUtil.newURI(testURL);
-  yield PlacesTestUtils.addVisits(testURI);
+      provider.on("deleteURI", handler);
+    });
 
-  // trigger DecayFrecency
-  PlacesUtils.history.QueryInterface(Ci.nsIObserver).
-    observe(null, "idle-daily", "");
-
-  yield promise;
-  provider.uninit();
+    let testURI = NetUtil.newURI(testURL);
+    yield PlacesTestUtils.addVisits(testURI);
+    yield PlacesUtils.history.remove(testURL);
+    yield deleteURIPromise;
+    provider.uninit();
+  }).then(done);
 };
 
-before(exports, function*() {
-  yield PlacesTestUtils.clearHistory();
+exports.test_Links_onManyLinksChanged = function(assert, done) {
+  Task.spawn(function*() {
+    let provider = PlacesProvider.links;
+    provider.init();
+
+    let promise = new Promise(resolve => {
+      let handler = () => {
+        assert.ok(true);
+        provider.off("manyLinksChanged", handler);
+        resolve();
+      };
+
+      provider.on("manyLinksChanged", handler);
+    });
+
+    let testURL = "https://example.com/toDelete";
+    let testURI = NetUtil.newURI(testURL);
+    yield PlacesTestUtils.addVisits(testURI);
+
+    // trigger DecayFrecency
+    PlacesUtils.history.QueryInterface(Ci.nsIObserver).
+      observe(null, "idle-daily", "");
+
+    yield promise;
+    provider.uninit();
+  }).then(done);
+};
+
+before(exports, function(name, assert, done) {
+  Task.spawn(function*() {
+    yield PlacesTestUtils.clearHistory();
+  }).then(done);
 });
 
 require("sdk/test").run(exports);
